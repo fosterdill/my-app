2f90df5861d750a13cfa22c79ca2ff38c7565607
Dylan Foster <fosterdill@gmail.com>
2024-05-30 20:49:39 -0400

    feat: github auth provider added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
@@ -6,6 +7,10 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
 });
