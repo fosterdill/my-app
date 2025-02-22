2a8ce1d849b7a6e7144e7f3f1154c2a74da6dd14
Dylan Foster <fosterdill@gmail.com>
2025-02-22 15:17:13 -0500

    sdf

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,3 +1,4 @@
+export const revalidate = 0;
 export function GET(request: Request) {
   console.log('Cron job executed at:', new Date().toISOString());
   return new Response('Cron job executed!', { status: 200 });
